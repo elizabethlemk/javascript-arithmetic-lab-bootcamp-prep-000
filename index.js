23ed404ebd285b322a223d417ba5acab3989db63
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-10-22 03:46:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b){
+  return a - b
+}
+function multiply(a, b){
+  return a * b
+}
+function divide(a, b){
+  return a / b
+}
+
+
+function inc(n) {
+  return n+= 1
+}
+
+function dec(n) {
+    return n-= 1
+}
+
+divideBy3()
+console.log(number)
+add5()
+console.log(number)
+number = 10
+add5()
+console.log(number)
+divideBy3()
+console.log(number)
