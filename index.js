6e4eafb0771b59ff26f6ec87357d8b872eb47b96
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-10-22 03:46:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,12 +20,4 @@ function dec(n) {
     return n-= 1
 }
 
-divideBy3()
-console.log(number)
-add5()
-console.log(number)
-number = 10
-add5()
-console.log(number)
-divideBy3()
-console.log(number)
+
